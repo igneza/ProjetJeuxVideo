58d2183de79bb4defcdd5e1e210ba25fcafd98e5
oliviernda <oliviernda@gmail.com>
2019-08-12 11:58:17 +0200

    Initial Commit

---
+++ ProjetJeuxVideo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjetJeuxVideo
+//
+//  Created by Nda Kwadzo Olivier on 12/08/2019.
+//  Copyright © 2019 nksolutions. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
